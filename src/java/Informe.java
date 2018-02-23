fe0ddd1ad2140e81f339ebde22450cc421f20595
Jorge Silva Borda <jorge.silva@tutorial.cl>
2018-02-23 15:32:43 -0300

    Referencias para amcharts
    Se cargan los archivos js y css para utilizar amcharts.
    El gráfico que se desea utilizar se encuentra en :
    https://www.amcharts.com/demos/multi-panel-horizon-chart/

---
+++ src/java/Informe.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+/**
+ * @author Jorge Silva Borda
+ */
+public class Informe extends HttpServlet {
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+
+	JSONObject entrada = new JSONObject(request.getParameter("datos"));
+	
+    }
+}
